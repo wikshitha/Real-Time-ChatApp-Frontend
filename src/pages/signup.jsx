5156a5c1e488789d25d6827377bbbddf395bd3b3
Wikshitha Umindu <uminduwikshitha@gmail.com>
2025-04-18 21:52:29 +0530

    work with signup page🌟

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState } from "react";
 
-const SignUpPage = () => {
+export default function SignUpPage() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function validateForm() {
+
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault();
+  }
+
   return (
-    <div> SignUpPage</div>
+  <div className="min-h-screen grid lg:grid-cols-2">
+  {/* left side */}
+  <div className="flex flex-col items-center justify-center sm:p-12">
+    <div className="w-full max-w-md space-y-8">
+    {/* LOGO */}
+    <div className="text-center mb-8">
+
+    </div>
+    </div>
+
+  </div>
+  </div>
   )
 }
-
-export default SignUpPage
